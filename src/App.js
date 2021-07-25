4380ee05ba796d2070082fd1b6ac54b310523a07
Garrett Grube <ggrube@quartethealth.com>
2021-07-25 15:42:30 -0400

    style update

--- src/App.js
+++ src/App.js
@@ -6,24 +6,25 @@ function App() {
     <div className="App">
       <h1>Garrett Grube</h1>
       <h2>Full-Stack Engineer • NYC</h2>
+      <br/>
       <p>
         <a href="https://react-redux-checkers.herokuapp.com/" target="blank">
-          A checkers game I built
-        </a>
-      </p>
-      <p>
-        <a href="https://www.linkedin.com/in/garrett-grube/" target="blank">
-          Linkedin
-        </a>
-      </p>
-      <p>
-        <a href="https://github.com/gdg10" target="blank">
-          Github
+          A Checkers Game
         </a>
       </p>
       <p>
         <a href="https://github.com/gdg10/garrett" target="blank">
-          The code for this website
+          This Website's Code
+        </a>
+      </p>
+      <p>
+        <a href="https://www.linkedin.com/in/garrett-grube/" target="blank">
+          Linkedin Profile
+        </a>
+      </p>
+      <p>
+        <a href="https://github.com/gdg10" target="blank">
+          Github Profile
         </a>
       </p>
     </div>
