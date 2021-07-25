72a810538298b208eaa1910dde73e0945cfdf3a5
Garrett Grube <ggrube@quartethealth.com>
2021-07-25 14:40:31 -0400

    added another link

--- src/App.js
+++ src/App.js
@@ -8,17 +8,22 @@ function App() {
       <h2>Full-Stack Engineer • NYC</h2>
       <p>
         <a href="https://react-redux-checkers.herokuapp.com/" target="blank">
-          a checkers game I built
+          A checkers game I built
         </a>
       </p>
       <p>
         <a href="https://www.linkedin.com/in/garrett-grube/" target="blank">
-          linkedin
+          Linkedin
         </a>
       </p>
       <p>
         <a href="https://github.com/gdg10" target="blank">
-          github
+          Github
+        </a>
+      </p>
+      <p>
+        <a href="https://github.com/gdg10/garrett" target="blank">
+          The code for this website
         </a>
       </p>
     </div>
