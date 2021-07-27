18e606c52c5069d358bb184cd495beafc6a5901b
Garrett Grube <ggrube@quartethealth.com>
2021-07-26 21:20:36 -0400

    update copy and link styling

--- src/App.test.js
+++ src/App.test.js
@@ -1,26 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('its renders my name', () => {
+test("its renders my name", () => {
   render(<App />);
   const myName = screen.getByText("Garrett Grube");
   expect(myName).toBeInTheDocument();
 });
 
-test('its renders a checkers link', () => {
+test("its renders a checkers link", () => {
   render(<App />);
-  const checkersLink = screen.getByText(/a checkers game I built/i);
+  const checkersLink = screen.getByText(/A Checkers Game/i);
   expect(checkersLink).toBeInTheDocument();
 });
 
-test('it renders a linkedin link', () => {
+test("it renders a linkedin link", () => {
   render(<App />);
-  const linkedinLink = screen.getByText(/linkedin/i);
+  const linkedinLink = screen.getByText(/Linkedin/i);
   expect(linkedinLink).toBeInTheDocument();
 });
 
-test('it renders a github link', () => {
+test("it renders a this website's code link", () => {
   render(<App />);
-  const githubLink = screen.getByText(/github/i);
+  const githubLink = screen.getByText(/This Website/i);
   expect(githubLink).toBeInTheDocument();
 });
